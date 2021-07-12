cfff0220b21fef8c506d59e8829bdb32e078e8db
Nithin Kumar <nithin9066@gmail.com>
2021-07-12 16:00:14 +0530

    added title

--- Student Management system/index.php
+++ Student Management system/index.php
@@ -132,7 +132,7 @@ elseif (isset($_POST['login'])) {
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
   <script src="jquery.js"></script>
-  <title></title>
+  <title>Student managment</title>
 </head>
 
 <body>
